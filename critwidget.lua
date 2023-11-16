bba1169f62b6b829bf923c19a0948c7bc1d19c58
Timur Mahmudov <makhmudov@masterdom.ru>
2023-11-16 16:59:19 +0300

    first commit

---
+++ critwidget.lua
@@ -0,0 +1,286 @@
+CritWidget = {
+	name = 'critwidget',
+	isLoaded = false
+}
+local CW = CritWidget
+local addon_name = CW.name
+local hasAPM = false
+
+CW.defaults_db = {
+	location = {
+		x = 0,
+		y = 97.4759368896
+	},
+	settings = {
+		global = false,
+		customScale = 20,
+		backgroundColor={0,0,0,0.8},
+		critType = {
+			spellCrit = false,
+			weaponCrit = true
+		},
+		hybrid = false,
+		levels = {
+			weapon = {
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 }
+			},
+			spell = {
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 },
+				{ color={1,1,1,1}, level = 0 }
+			}
+		},
+		renderTick = 500
+	}
+}
+
+local currentCritLevel = 0
+local inspirationalBanter = {
+    'You really need to think that you are good at this.',
+    'Honestly, if you were any slower, you’d be going backward.',
+    'Amassing a 24 man group will increase your survivability.',
+    'Impen is kind of important, but so is being good.',
+    'Have you tried PVE?',
+    'Consider the mongoose, would they have put up a fight like this? Get back in there champ, and keep the claws up.',
+    'Wow. We are actually speechless.',
+    'Have you tried actually trying?',
+    'Have you tried resetting your router?',
+    'Next time, stick them with the pointy end.',
+    'Impen really really really really helps.',
+    'Next time, don’t make insulting yo moma emotes, it just angers them.',
+    'Actually having damage.',
+    'Lift with your legs, not your back.',
+    'If there are two coconuts, and you take one away, the remaining coconut would still last longer than you did.',
+    'Equip more Earthgores.',
+    'Search for tutorials on YouTube, you may just get a tiny bit better.',
+    'If you happened to be streaming on Twitch during this, it is all kind of funny if you think about it.',
+    'You should listen to encouraging music when fighting, like Bon Jovi.',
+    'Hold block a bit more.',
+    'If only you had gotten that other Artifact',
+    'Stop trying and join the Greybeards',
+    'Maybe Crafting Writs are more your level',
+    'The objective is to stay alive',
+    'Have you considered doing normal level dungeons instead?',
+	'Oops, you died so fast - I didn\'t have time to generate tips',
+	'You could make a TikTok out of that and get some feedback'
+}
+
+function CW.SaveLocation()
+	local critwidgetUIControl = CW.UI.control
+    CW.db.location.x = critwidgetUIControl:GetLeft()
+    CW.db.location.y = critwidgetUIControl:GetTop()
+end
+
+local function legacyRender( inital )
+	local settings = CW.db.settings
+	local levelSettings = settings.levels
+
+	local color = {1,1,1,1}
+	local critLevel = math.floor((GetPlayerStat(STAT_CRITICAL_STRIKE)/219*10) + 0.5)/10
+	local critGroup = levelSettings.weapon
+
+	if settings.critType.spellCrit then
+		critLevel = math.floor((GetPlayerStat(STAT_SPELL_CRITICAL)/219*10) + 0.5)/10
+		critGroup = levelSettings.spell
+	end
+
+	for i in pairs(critGroup) do
+
+		local alertLevel = tonumber(critGroup[i].level)
+		alertLevel = alertLevel or 0 -- = defaults.settings.levels.weapon/spell.level
+		if alertLevel ~= 0 and critLevel >= alertLevel then
+			color = critGroup[i].color
+		end
+	
+	end
+
+	if currentCritLevel ~= critLevel or inital then
+		currentCritLevel = critLevel
+		local critwidgetUI = CW.UI
+		local critLabel = critwidgetUI.Crit
+
+		local r,g,b,a = unpack(color)
+
+		critLabel:SetText(tostring(critLevel)..'%')
+		critLabel:SetColor(r,g,b,a)
+		a = 0.5
+		critwidgetUI.Border:SetEdgeColor(r, g, b, a)
+	end
+
+end
+
+local function hybridRender( inital )
+	local settings = CW.db.settings
+	local levelSettings = settings.levels
+	local critwidgetUI = CW.UI
+	local statLabelValue = 'WC'
+
+	local color = {1,1,1,1}
+	local critLevel = math.floor((GetPlayerStat(STAT_CRITICAL_STRIKE)/219*10) + 0.5)/10
+	local critGroup = levelSettings.weapon
+	local spellCritLevel = math.floor((GetPlayerStat(STAT_SPELL_CRITICAL)/219*10) + 0.5)/10
+
+	if spellCritLevel > critLevel then
+		critLevel = spellCritLevel
+		critGroup = levelSettings.spell
+		statLabelValue = 'SC'
+	end
+
+	for i in pairs(critGroup) do
+
+		local alertLevel = tonumber(critGroup[i].level)
+		alertLevel = alertLevel or 0 -- = defaults.settings.levels.weapon/spell.level
+		if alertLevel ~= 0 and critLevel >= alertLevel then
+			color = critGroup[i].color
+		end
+	
+	end
+
+	if currentCritLevel ~= critLevel or inital then
+		currentCritLevel = critLevel
+		local r,g,b,a = unpack(color)
+		local crit = critwidgetUI.Crit
+		local critBorder = critwidgetUI.Border
+		local critLabel = critwidgetUI.CritLabel
+
+		crit:SetText(tostring(critLevel)..'%')
+		critLabel:SetText(statLabelValue)
+		critLabel:SetColor(r,g,b,a)
+		critBorder:SetEdgeColor(r, g, b, 0.5)
+	end
+
+end
+
+local function render( init )
+
+	if CW.db.settings.hybrid then
+		hybridRender( init )
+	else
+		legacyRender( init )
+	end
+
+end
+CW.Render = render
+
+function CW.CustomScale(value)
+
+	local critwidgetUI = CW.UI
+
+	critwidgetUI.Crit:SetFont('$(GAMEPAD_BOLD_FONT)|'..tostring( 28 + (28/100*value) )..'|thin-outline')
+	critwidgetUI.CritLabel:SetFont('$(BOLD_FONT)|'..tostring( 12 + (12/100*value) )..'|thin-outline')
+
+	local newWidth, newHeight = 80 + (80/100*value), 40 + (40/100*value)
+	critwidgetUI.BG:SetDimensions( newWidth, newHeight )
+	critwidgetUI.control:SetDimensions( newWidth, newHeight )
+	critwidgetUI.Border:SetDimensions( 83 + (83/100*value), 43 + (43/100*value) )
+
+end
+
+-- --------------------
+-- Addon initialization
+-- --------------------
+local function GS_Initialize()
+
+	CW.isLoaded = true
+	local settings = CW.db.settings
+
+	CW.UI = {
+		control 	= CritWidgetUI,
+		Crit 		= CritWidgetUICrit,
+		BG 			= CritWidgetUIBG,
+		CritLabel 	= CritWidgetUICritLabel,
+		Border 		= CritWidgetUIBorder
+	}
+	local critwidgetUI = CW.UI
+
+	if settings.critType.spellCrit then
+		currentCritLevel = math.floor((GetPlayerStat(STAT_SPELL_CRITICAL)/219*10) + 0.5)/10
+		critwidgetUI.CritLabel:SetText('SC')
+	end
+	critwidgetUI.Crit:SetText(tostring(currentCritLevel)..'%')
+
+	EVENT_MANAGER:RegisterForUpdate(addon_name..'Render',settings.renderTick,render)
+	EVENT_MANAGER:UnregisterForEvent(addon_name, EVENT_ADD_ON_LOADED)
+
+	local critwidgetUIBG = critwidgetUI.BG
+	critwidgetUIBG:SetEdgeColor(ZO_ColorDef:New(0,0,0,0):UnpackRGBA())
+	critwidgetUI.Border:SetCenterColor(ZO_ColorDef:New(0,0,0,0):UnpackRGBA())
+	critwidgetUIBG:SetCenterColor(unpack(CW.db.settings.backgroundColor))
+
+	CW.CustomScale(settings.customScale)
+
+	render(true)
+
+	local dbLocation = CW.db.location
+	local critwidgetUIControl = critwidgetUI.control
+
+	critwidgetUIControl:ClearAnchors()
+    critwidgetUIControl:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, dbLocation.x, dbLocation.y)
+
+    if not hasAPM then
+	    EVENT_MANAGER:RegisterForEvent(addon_name, EVENT_PLAYER_DEAD, function()
+	        if IsInCampaign() or IsActiveWorldBattleground() then
+	            zo_callLater(function()
+	                local text = inspirationalBanter[math.random(#inspirationalBanter)]
+	                ZO_DeathRecapScrollContainerScrollChildHintsContainerHints1Text:SetText(text)
+	            end,3000)
+	        end
+
+	    end)
+    end
+
+    local fragment = ZO_HUDFadeSceneFragment:New(critwidgetUIControl, nil, 0)
+	HUD_SCENE:AddFragment(fragment)
+	HUD_UI_SCENE:AddFragment(fragment)
+
+end
+-- local addonInit = GS.Initialize
+
+local function OnPlayerActivated(eventCode)
+    EVENT_MANAGER:UnregisterForEvent(addon_name, eventCode)
+    GS_Initialize()
+end
+
+local function GS_OnAddOnLoaded(event, addOnName)
+	--Other addons check
+	if addOnName == 'APMeter' then
+		hasAPM = true
+	else
+		if addOnName == addon_name then
+			EVENT_MANAGER:UnregisterForEvent(addon_name, EVENT_ADD_ON_LOADED)
+
+			-- Migration script
+			if( CritWidgetSettings and CritWidgetSettings['Default'] and CritWidgetSettings['Default'][GetDisplayName()] and CritWidgetSettings['Default'][GetDisplayName()][GetUnitName("player")] ) then
+				
+				local mainAccountDataRecord = CritWidgetSettings['Default'][GetDisplayName()]
+				local legacyDataRecord = mainAccountDataRecord[GetUnitName("player")]
+
+				mainAccountDataRecord[GetCurrentCharacterId()] = {}
+				mainAccountDataRecord[GetCurrentCharacterId()][GetWorldName()] = legacyDataRecord
+
+				CritWidgetSettings['Default'][GetDisplayName()][GetUnitName("player")] = nil
+			end
+
+			CW.db = ZO_SavedVars:NewCharacterIdSettings("CritWidgetSettings", 2, GetWorldName(), CW.defaults_db, nil)
+
+			-- if GS.db.settings.global then
+			-- 	GS.db = ZO_SavedVars:NewAccountWide('CritMeterSettings', 2, nil, GS.db_defaults)
+			-- 	GS.db.settings.global = true
+			-- end
+
+			CW.buildSettingsMenu()
+
+			EVENT_MANAGER:RegisterForEvent(addon_name, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
+		end
+	end
+end
+-- --------------------
+-- Attach Listeners
+-- --------------------
+EVENT_MANAGER:RegisterForEvent(addon_name, EVENT_ADD_ON_LOADED, GS_OnAddOnLoaded)
---
+++ settings.lua
@@ -0,0 +1,218 @@
+local CW = CritWidget
+
+local function buildLAMSettingsMenu()
+	-- --------------------
+	-- Create Settings
+	-- --------------------
+	local optionsData = {}
+	local LAM2 = LibAddonMenu2
+	local settings = CW.db.settings
+	local defaults = CW.defaults_db.settings
+
+	local panelData = {
+		 type = "panel",
+		 name = "CritWidget",
+		 version = "1.0.0",
+		 author = '|c00a313Ghostbane, @Zempak|r',
+		 website = 'https://www.esoui.com/downloads/info1924-GreyskullWeaponSpellDamageMeter.html'
+	}
+
+	-- optionsData[#optionsData+1] = {
+    --     type = 'checkbox',
+    --     name = 'Account Wide Settings',
+    --     tooltip = 'Check for account wide addon settings',
+    --     getFunc = function() return GS.db.settings.global end,
+    --     setFunc = function(value)
+    
+    --         if value then
+    --             GS.db = ZO_SavedVars:NewAccountWide('GreyskullSettings', 2, nil, GS.defaults_db)
+    --         else
+    --             GS.db = ZO_SavedVars:NewCharacterIdSettings('GreyskullSettings', 2, GetWorldName(), GS.defaults_db, nil)
+    --         end
+    
+    --         GS.db.settings.global = value
+    
+    --     end,
+    --     -- requiresReload = true,
+    --     default = false
+    -- }
+
+
+	optionsData[#optionsData+1] = {
+		type = "dropdown",
+		name = "Crit type:",
+		tooltip = "Choose your crit type",
+		choices = {'Weapon Crit','Spell Crit','Hybrid'},
+		getFunc = function()
+			if settings.hybrid then
+				return 'Hybrid'
+			elseif settings.critType.spellCrit then
+				return 'Spell Crit'
+			else
+				return 'Weapon Crit'
+			end
+		end,
+		setFunc = function(choice)
+			
+			local critType = settings.critType
+
+			if choice == 'Hybrid' then
+				settings.hybrid = true
+				critType.spellCrit = false
+				critType.weaponCrit = false
+			else
+				settings.hybrid = false
+
+				if choice == 'Weapon Crit' then
+					critType.spellCrit = false
+					critType.weaponCrit = true
+					CW.UI.CritLabel:SetText('WC')
+				else
+					critType.spellCrit = true
+					critType.weaponCrit = false
+					CW.UI.CritLabel:SetText('SC')
+				end
+			end
+		end
+	}
+
+	optionsData[#optionsData+1] = {
+		type = "colorpicker",
+		name = "Background Color",
+		tooltip = "Container BG Color",
+		getFunc = function() return unpack(settings.backgroundColor) end,
+		setFunc = function(r,g,b,a)
+			settings.backgroundColor = {r,g,b,a}
+			CW.UI.BG:SetCenterColor(r,g,b,a)
+		end,
+		default = unpack(defaults.backgroundColor)
+	}
+
+	optionsData[#optionsData+1] = {
+		type = "slider",
+		name = "Custom Scale UI",
+		tooltip = "Enlarge by %",
+		min = 0,
+		max = 100,
+		getFunc = function()
+			return settings.customScale
+		end,
+		setFunc = function(value)
+			CW.CustomScale(value)
+			settings.customScale = value
+		end,
+		default = defaults.customScale
+	}
+
+	optionsData[#optionsData+1] = {
+		type = "description",
+		title = "",
+		text = [[
+		]]
+	}
+	optionsData[#optionsData+1] = {
+		type = "header",
+		name = "Custom Color Notifcations",
+	}
+	optionsData[#optionsData+1] = {
+		type = "description",
+		text = [[You can set a crit value, and assign it a color. Whenever your Weapon/Spell Crit goes past this number, your meter value will change to this colour as an easier in-combat reference to a particular damage increase proc
+		]]
+	}
+
+	local weaponCritOptions = {}
+
+	for i in pairs(defaults.levels.weapon) do
+		weaponCritOptions[#weaponCritOptions+1] = {
+			type = "editbox",
+			name = "Weapon Crit Level #"..i,
+			tooltip = "Crit level for Level #"..i,
+			getFunc = function() return settings.levels.weapon[i].level end,
+			setFunc = function(level) settings.levels.weapon[i].level = level end,
+			default = defaults.levels.weapon[i].level,
+			width = 'half'
+		}
+		weaponCritOptions[#weaponCritOptions+1] = {
+			type = "colorpicker",
+			tooltip = "Color for Level #"..i,
+			getFunc = function() return unpack(settings.levels.weapon[i].color) end,
+			setFunc = function(r,g,b,a) settings.levels.weapon[i].color = {r,g,b,a} end,
+			default = unpack(defaults.levels.weapon[i].color),
+			width = 'half'
+		}
+	end
+
+	local spellCritOptions = {}
+
+	for i in pairs(defaults.levels.spell) do
+		spellCritOptions[#spellCritOptions+1] = {
+			type = "editbox",
+			name = "Spell Crit Level #"..i,
+			tooltip = "Crit level for Level #"..i,
+			getFunc = function() return settings.levels.spell[i].level end,
+			setFunc = function(level) settings.levels.spell[i].level = level end,
+			default = defaults.levels.spell[i].level,
+			width = 'half'
+		}
+		spellCritOptions[#spellCritOptions+1] = {
+			type = "colorpicker",
+			tooltip = "Color for Level #"..i,
+			getFunc = function() return unpack(settings.levels.spell[i].color) end,
+			setFunc = function(r,g,b,a) settings.levels.spell[i].color = {r,g,b,a} end,
+			default = unpack(defaults.levels.spell[i].color),
+			width = 'half'
+		}
+	end
+
+	optionsData[#optionsData + 1] = {
+		type = "submenu",
+		name = 'Weapon Crit Alerts',
+		reference = "Weapon_Crit_Options_Submenu",
+		controls = weaponCritOptions
+	}
+
+	optionsData[#optionsData + 1] = {
+		type = "submenu",
+		name = 'Spell Crit Alerts',
+		reference = "Spell_Crit_Options_Submenu",
+		controls = spellCritOptions
+	}
+
+	optionsData[#optionsData+1] = {
+		type = "description",
+		title = "",
+		text = [[
+		]]
+	}
+	optionsData[#optionsData+1] = {
+		type = "header",
+		name = "Advanced Setting",
+	}
+	optionsData[#optionsData+1] = {
+		type = "description",
+		title = "",
+		text = [[The milliseconds for the loop that renders the damage of the meter. You can select the value based on what is the best performance for you. Default is 500ms
+		]]
+	}
+	optionsData[#optionsData+1] = {
+		type = "slider",
+		name = "Render Interval Setting",
+		tooltip = "How fast should the meter re-render",
+		min = 200,
+		max = 3000,
+		getFunc = function()
+			return settings.renderTick
+		end,
+		setFunc = function(value)
+			EVENT_MANAGER:UnregisterForUpdate(addon_name..'Render')
+			EVENT_MANAGER:RegisterForUpdate(addon_name..'Render',value, CW.Render)
+			settings.renderTick = value
+		end,
+		default = defaults.renderTick
+	}
+
+	LAM2:RegisterAddonPanel("CritWidgetOptions", panelData)
+	LAM2:RegisterOptionControls("CritWidgetOptions", optionsData)
+end
+
+CW.buildSettingsMenu = buildLAMSettingsMenu
